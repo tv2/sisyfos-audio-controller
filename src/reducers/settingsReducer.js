8201c75ebdbdc7ef5bdef73967ee4fc549934d0e
olzzon <github@olzzon.dk>
2019-02-07 08:53:51 +0100

    Preparing Settings (moving adjustable things away form DEFAULTS.js)

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -1,35 +1,37 @@
-const defaultSettingsReducerState = [{
-    oscPort: 8000,
-    machineOscPort: 8001,
-    mixer: {
+const defaultSettingsReducerState = [
+    {
+        showSnaps: false,
+        showSettingsPage: false,
+        oscPort: 8000,
+        machineOscIp: "0.0.0.0",
+        machineOscPort: 8001,
         numberOfChannels: 8,
         numberOfSnaps: 8,
-        showSnaps: false,
         fader: {
-            min: 0,
-            max: 1,
-            zero: 0.75,
-            setp: 0.01
+        min: 0,
+        max: 1,
+        zero: 0.75,
+        setp: 0.01,
         },
         meter: {
-            min: 0,
-            max: 0,
-            zero: 0.75,
-            test: 0.6
-        }
-    }
-}];
+        min: 0,
+        max: 0,
+        zero: 0.75,
+        test: 0.6,
+        },
+    },
+];
 
-export const settings = ((state = defaultSettingsReducerState, action) => {
-    let { ...nextState } = state;
+export const settings = (state = defaultSettingsReducerState, action) => {
+    let {...nextState} = state;
 
-    switch(action.type) {
-        case 'UPDATE_SETTINGS':
-            return nextState;
+    switch (action.type) {
+        case 'TOGGLE_SHOW_SETTINGS':
+        return nextState;
         case 'TOGGLE_SHOW_SNAPS':
-            nextState[0].showSnaps = !nextState[0].showSnaps;
-            return nextState;
+        nextState[0].showSnaps = !nextState[0].showSnaps;
+        return nextState;
         default:
-            return nextState;
+        return nextState;
     }
-});
+};
