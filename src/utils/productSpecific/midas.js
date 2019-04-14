3d54ddacf8746e6d75b43f342c8833e1d02912de
olzzon <github@olzzon.dk>
2019-04-14 09:30:27 +0200

    Midas meters working.

--- src/utils/productSpecific/midas.js
+++ src/utils/productSpecific/midas.js
@@ -4,14 +4,21 @@ export const midasMeter = (message) => {
     //Test data from Behringer:
     //message = [40, 0, 0, 0, 133, 157, 183, 156, 72, 154, 101, 157, 229, 162, 241, 158, 253, 162, 156, 162, 131, 162, 253, 162, 81, 162, 29, 162, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 0, 128, 223, 157, 223, 157, 223, 157, 223, 157];
 
+    const headerData = 4;
     let uint8bytes = Uint8Array.from(message[0]);
     let dataview = new DataView(uint8bytes.buffer);
+    //console.log(dataview);
 
     for (let i=0; i < store.settings[0].numberOfChannels; i++) {
+/*
+        if (i===1) {
+            console.log(dataview.getFloat32(4*(i)+headerData , true))
+        }
+*/
         window.storeRedux.dispatch({
             type:'SET_VU_LEVEL',
             channel: i,
-            level: (dataview.getInt16(2*(i+2) , true) + 8000)/8000
+            level: (dataview.getFloat32(4*i+headerData , true))
         });
     }
 };
