42b8b5dcc59a468141a5203a39d37f24dfa464fc
Olzzon <github@olzzon.dk>
2019-10-10 14:23:25 +0200

    feat/route-all-button: Clear All Fader-Channel assignments working

--- src/components/ChannelRouteSettings.tsx
+++ src/components/ChannelRouteSettings.tsx
@@ -9,7 +9,7 @@ import CcgChannelSettings from './CcgChannelSettings';
 const { dialog } = require('electron').remote;
 
 interface IChannelSettingsInjectProps {
-	label: string,
+    label: string,
     selectedProtocol: string,
     numberOfChannelsInType: Array<number>,
     channel: Array<any>
@@ -17,7 +17,7 @@ interface IChannelSettingsInjectProps {
 }
 
 interface IChannelProps {
-	faderIndex: number
+    faderIndex: number
 }
 
 class ChannelRouteSettings extends React.PureComponent<IChannelProps & IChannelSettingsInjectProps & Store> {
@@ -26,21 +26,21 @@ class ChannelRouteSettings extends React.PureComponent<IChannelProps & IChannelS
 
     constructor(props: any) {
         super(props);
-		this.faderIndex = this.props.faderIndex;
-		this.mixerProtocol = MixerProtocolPresets[this.props.selectedProtocol];
+        this.faderIndex = this.props.faderIndex;
+        this.mixerProtocol = MixerProtocolPresets[this.props.selectedProtocol];
     }
 
     handleAssignChannel(channel: number, event: any) {
         let faderAssign = this.faderIndex
         let assignedFader = this.props.channel[channel].assignedFader
-        let assignedFaderLabel = (assignedFader >= 0) 
-            ? this.props.fader[assignedFader].label 
+        let assignedFaderLabel = (assignedFader >= 0)
+            ? this.props.fader[assignedFader].label
             : 'undefined'
-        assignedFaderLabel = (assignedFaderLabel === '') 
+        assignedFaderLabel = (assignedFaderLabel === '')
             ? String(assignedFader + 1)
             : assignedFaderLabel
 
-        if (event.target.checked === false) { 
+        if (event.target.checked === false) {
             const options = {
                 type: 'question',
                 buttons: ['Yes', 'Cancel'],
@@ -56,8 +56,8 @@ class ChannelRouteSettings extends React.PureComponent<IChannelProps & IChannelS
             faderAssign = -1
         } else {
             let detail = (assignedFader < 0) ? 'NOT CURRENTLY ASSIGNED'
-                : 'CHANNEL ' + String(channel + 1) + ' IS CURRENTLY CONNECTED TO FADER ' + String(assignedFaderLabel) 
-                
+                : 'CHANNEL ' + String(channel + 1) + ' IS CURRENTLY CONNECTED TO FADER ' + String(assignedFaderLabel)
+
             const options = {
                 type: 'question',
                 buttons: ['Yes', 'Cancel'],
@@ -80,36 +80,73 @@ class ChannelRouteSettings extends React.PureComponent<IChannelProps & IChannelS
         return true;
     }
 
-	handleClose = () => {
-		this.props.dispatch({
-			type: 'TOGGLE_SHOW_OPTION',
-			channel: this.faderIndex
-		});
-	}
+    handleClearRouting() {
+        const options = {
+            type: 'question',
+            buttons: ['Yes', 'Cancel'],
+            defaultId: 1,
+            title: 'WARNING',
+            message: 'WARNING!!!!!',
+            detail: 'This will remove all Fader-Channel assignments',
+        };
+        let response = dialog.showMessageBoxSync(options)
+        if (response === 0) {
+            this.props.channel.forEach((channel: any, index: number) => {
+                this.props.dispatch({
+                    type: 'SET_ASSIGNED_FADER',
+                    channel: index,
+                    faderNumber: -1
+                });
+            })
+        }
+        return true
+    }
+
+    handle11Routing() {
+        const options = {
+            type: 'question',
+            buttons: ['Yes', 'Cancel'],
+            defaultId: 1,
+            title: 'WARNING',
+            message: 'WARNING!!!!!',
+            detail: 'This will reassign all Faders 1:1 to Channels',
+        };
+        let response = dialog.showMessageBoxSync(options)
+        return true
+    }
+
+    handleClose = () => {
+        this.props.dispatch({
+            type: 'TOGGLE_SHOW_OPTION',
+            channel: this.faderIndex
+        });
+    }
 
     render() {
         if (this.props.selectedProtocol.includes("caspar")) {
             return (
-                <CcgChannelSettings channelIndex= {this.props.faderIndex}/>
-                )
-            }
+                <CcgChannelSettings channelIndex={this.props.faderIndex} />
+            )
+        }
         else {
             return (
                 <div className="channel-settings-body">
                     <h2>{this.props.label || ("FADER " + (this.faderIndex + 1))}</h2>
                     <button className="close" onClick={() => this.handleClose()}>X</button>
 
-
+                    <button onClick={() => this.handleClearRouting()}>CLEAR ALL</button>
+                    <button onClick={() => this.handle11Routing()}>ROUTE 1:1</button>
+                    <hr />
                     {this.props.channel.map((channel: any, index: number) => {
                         return <h4 key={index}>
-                            {(" Channel " + (index + 1) + " : " )}
+                            {(" Channel " + (index + 1) + " : ")}
                             <input
                                 type="checkbox"
-                                checked={ this.props.channel[index].assignedFader === this.faderIndex }
+                                checked={this.props.channel[index].assignedFader === this.faderIndex}
                                 onChange={(event) => this.handleAssignChannel(index, event)}
                             />
-                            {this.props.channel[index].assignedFader >=0 
-                                ? ( "   (Fader " + (this.props.channel[index].assignedFader + 1) + ")") 
+                            {this.props.channel[index].assignedFader >= 0
+                                ? ("   (Fader " + (this.props.channel[index].assignedFader + 1) + ")")
                                 : ' (not assigned)'}
                         </h4>
                     })
