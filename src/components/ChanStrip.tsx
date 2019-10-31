6e15054982e26a9fdaf499d6596dd6a3a5c6a753
Olzzon <github@olzzon.dk>
2019-10-31 08:31:29 +0100

    feat/offtube: add channel stip component

---
+++ src/components/ChanStrip.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+import '../assets/css/ChanStrip.css';
+import { MixerProtocolPresets } from '../constants/MixerProtocolPresets';
+import { IMixerProtocolGeneric } from '../constants/MixerProtocolInterface';
+import { Store } from 'redux';
+import { connect } from 'react-redux';
+
+interface IChanStripInjectProps {
+    label: string,
+    selectedProtocol: string,
+    numberOfChannelsInType: Array<number>,
+    channel: Array<any>
+    fader: Array<any>
+}
+
+interface IChanStripProps {
+    faderIndex: number
+}
+
+class ChanStrip extends React.PureComponent<IChanStripProps & IChanStripInjectProps & Store> {
+    faderIndex: number;
+    mixerProtocol: IMixerProtocolGeneric;
+
+    constructor(props: any) {
+        super(props);
+        this.faderIndex = this.props.faderIndex;
+        this.mixerProtocol = MixerProtocolPresets[this.props.selectedProtocol];
+    }
+
+    handleAssignChannel(channel: number, event: any) {
+        return true;
+    }
+
+    handleClearRouting() {
+        return true
+    }
+
+    handle11Routing() {
+        return true
+    }
+
+    handleClose = () => {
+        this.props.dispatch({
+            type: 'TOGGLE_SHOW_CHAN_STRIP',
+            channel: -1
+        });
+    }
+
+    render() {
+        return (
+            <div className="chan-strip-body">
+                <h2>
+                    CHANNEL STRIP 
+                    <br/>
+                    {this.props.label || ("FADER " + (this.faderIndex + 1))}</h2>
+                <button 
+                    className="close"
+                    onClick={() => this.handleClose()}
+                >X</button>
+                <button 
+                    className="button"
+                    onClick={() => this.handleClearRouting()}
+                >NOTHING</button>
+                <button 
+                    className="button"
+                    onClick={() => this.handle11Routing()}
+                >NOTHING ELSE</button>
+                <hr />
+            </div>
+        )
+    }
+
+}
+
+const mapStateToProps = (state: any, props: any): IChanStripInjectProps => {
+    return {
+        label: state.faders[0].fader[props.faderIndex].label,
+        selectedProtocol: state.settings[0].mixerProtocol,
+        numberOfChannelsInType: state.settings[0].numberOfChannelsInType,
+        channel: state.channels[0].channel,
+        fader: state.faders[0].fader,
+    }
+}
+
+export default connect<any, IChanStripInjectProps>(mapStateToProps)(ChanStrip) as any;
