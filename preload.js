b87c859525ee22c7215857e829786dd10b2d6d47
Olzzon <github@olzzon.dk>
2019-12-03 13:22:27 +0100

    fix: upload preload.js file after it was moved

---
+++ preload.js
@@ -0,0 +1,36 @@
+console.log('preloading')
+
+const remote = require('electron').remote
+const { dialog } = require('electron').remote
+const { getPath } = require('electron').remote.app
+
+
+global.aaaasper = 'IT´s preloaded'
+global.fs = remote.require('fs')
+global.osc = remote.require('osc')
+global.net = remote.require('net')
+global.emberplus = remote.require('emberplus')
+global.casparcgconnection = remote.require('casparcg-connection')
+global.dialog = dialog
+global.getPath = getPath
+//global.SettingsStorage = './utils/SettingsStorage.ts'
+
+
+console.log('PreLoad executed')
+
+global.electron = {
+  ipc: {
+    invoke (channel, ...args) {
+      return ipcRenderer.invoke(channel, ...args)
+    },
+    send (msg, arg) {
+      ipcRenderer.send(msg, arg)
+    },
+    on (channel, cb) {
+      ipcRenderer.on(channel, cb)
+    },
+    off (channel, cb) {
+      ipcRenderer.off(channel, cb)
+    }
+  }
+}
