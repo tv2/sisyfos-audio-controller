2cf68e592d386b17a84c7984c0df5c31ad2e29a8
Olzzon <github@olzzon.dk>
2019-12-11 14:02:34 +0100

    feat/move-to-main: preload cleanup

--- preload.js
+++ preload.js
@@ -1,11 +1,9 @@
 console.log('preloading')
 
-const remote = require('electron').remote
 const { ipcRenderer } = require('electron')
 const { dialog } = require('electron').remote
 const { getPath } = require('electron').remote.app
 
-global.fs = remote.require('fs')
 global.dialog = dialog
 global.getPath = getPath
 global.ipcRenderer = ipcRenderer
@@ -13,21 +11,4 @@ global.ipcRenderer = ipcRenderer
 //global.SettingsStorage = './utils/SettingsStorage.ts'
 
 global.aaaa = 'PRELOAD WAS EXECUTED'
-console.log('PreLoad executed')
-
-/*
-let listener;
-const bridge = {
-   send: data => ipcRenderer.send('from-renderer', data),
-   onMessage: callback => listener = callback 
-}
-
-ipcRenderer.on('to-renderer', (event, arg) => {
-  if (listener) {
-     listener(arg);
-  } else {
-     console.warn('No listener');
-  }
-});
-
-global.bridge = bridge;*/
+console.log('PreLoad is executed')
