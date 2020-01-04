5bc7edd3cad9b742cbc07a820aa0cb14746513e9
Olzzon <github@olzzon.dk>
2020-01-04 07:47:13 +0100

    refactor: add file importClasses.ts

---
+++ server/importClasses.ts
@@ -0,0 +1,23 @@
+import { MixerProtocolPresets, MixerProtocolList } from './constants/MixerProtocolPresets'
+import { MixerGenericConnection } from './utils/MixerConnection';
+import { AutomationConnection } from './utils/AutomationConnection';
+import { HuiMidiRemoteConnection } from './utils/HuiMidiRemoteConnection';
+
+import { state } from './reducers/store'
+let mixerProtocolPresets = MixerProtocolPresets
+let mixerProtocolList = MixerProtocolList
+
+let mixerGenericConnection = new MixerGenericConnection();
+let automationConnection = new AutomationConnection();
+let huiRemoteConnection: HuiMidiRemoteConnection | null = null
+if (state.settings[0].enableRemoteFader){
+    huiRemoteConnection = new HuiMidiRemoteConnection();
+}
+
+export { 
+    mixerProtocolList, 
+    mixerProtocolPresets, 
+    mixerGenericConnection, 
+    automationConnection,
+    huiRemoteConnection
+}
