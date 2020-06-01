47b54d20ada009d2094f543a6f6b1e9715bffe6d
olzzon <olzzon@Kaspers-MacBook-Pro.local>
2020-06-01 12:39:51 +0200

    fix: midas - x32 metering was pr channel not pr assignedFader

--- server/utils/mixerConnections/productSpecific/midas.ts
+++ server/utils/mixerConnections/productSpecific/midas.ts
@@ -6,13 +6,16 @@ import {
     SET_VU_LEVEL,
 } from '../../../reducers/faderActions'
 import { SOCKET_SET_ALL_VU } from '../../../constants/SOCKET_IO_DISPATCHERS'
+import { IChannel } from '../../../reducers/channelsReducer'
 
 export const midasMeter = (message: any) => {
     const DATA_OFFSET = 4
     let uint8bytes = Uint8Array.from(message[0])
     let dataview = new DataView(uint8bytes.buffer)
-    let vuMeters = []
-    let vuReductionMeters = []
+    let vuMeters: number[] = new Array(state.settings[0].numberOfFaders)
+    let vuReductionMeters: number[] = new Array(
+        state.settings[0].numberOfFaders
+    )
     let level: number
     let reductionLevel: number
     let numberOfChannels = state.settings[0].numberOfChannelsInType[0]
@@ -21,18 +24,20 @@ export const midasMeter = (message: any) => {
         level = dataview.getFloat32(4 * i + DATA_OFFSET, true)
         reductionLevel = dataview.getFloat32(4 * (i + 64) + DATA_OFFSET, true)
 
-        vuMeters.push(level)
+        vuMeters[state.channels[0].channel[i].assignedFader] = level
         store.dispatch({
             type: SET_VU_LEVEL,
-            channel: i,
+            channel: state.channels[0].channel[i].assignedFader,
             level: level,
         })
 
-        vuReductionMeters.push(reductionLevel)
         reductionLevel = 1 - reductionLevel
+        vuReductionMeters[
+            state.channels[0].channel[i].assignedFader
+        ] = reductionLevel
         store.dispatch({
             type: SET_VU_REDUCTION_LEVEL,
-            channel: i,
+            channel: state.channels[0].channel[i].assignedFader,
             level: reductionLevel,
         })
     }
