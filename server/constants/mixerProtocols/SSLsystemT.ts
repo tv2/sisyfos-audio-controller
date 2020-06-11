93b4ca79cd9664878f73eba9e35dff36d9530e2c
olzzon <olzzon@Kaspers-MacBook-Pro.local>
2020-06-11 18:46:44 +0200

    chore: SSL protocol cleanup

--- server/constants/mixerProtocols/SSLsystemT.ts
+++ server/constants/mixerProtocols/SSLsystemT.ts
@@ -13,11 +13,6 @@ export const SSLSystemT: IMixerProtocol = {
     initializeCommands: [
         {
             mixerMessage: 'f1 04 00 00 00 {channel}',
-            value: 0,
-            type: '',
-            min: 0,
-            max: 1,
-            zero: 0.75,
         },
     ],
     channelTypes: [
@@ -26,99 +21,41 @@ export const SSLSystemT: IMixerProtocol = {
             channelTypeColor: '#2f2f2f',
             fromMixer: {
                 CHANNEL_OUT_GAIN: [emptyMixerMessage()], // Handled by SSLMixerconnection
-                CHANNEL_VU: [emptyMixerMessage()], // Not implemented in SSL Automation protocol yet
-                CHANNEL_VU_REDUCTION: [emptyMixerMessage()],
-                CHANNEL_NAME: [emptyMixerMessage()],
-                PFL: [emptyMixerMessage()],
-                NEXT_SEND: [emptyMixerMessage()],
-                THRESHOLD: [emptyMixerMessage()],
-                RATIO: [emptyMixerMessage()],
-                DELAY_TIME: [emptyMixerMessage()],
-                LOW: [emptyMixerMessage()],
-                LO_MID: [emptyMixerMessage()],
-                MID: [emptyMixerMessage()],
-                HIGH: [emptyMixerMessage()],
-                AUX_LEVEL: [emptyMixerMessage()],
                 CHANNEL_MUTE_ON: [
                     {
                         mixerMessage: 'f1 04 00 01 00 {channel}',
-                        value: 0,
-                        type: '',
-                        min: 0,
-                        max: 1,
-                        zero: 0.75,
                     },
                 ],
-                CHANNEL_MUTE_OFF: [emptyMixerMessage()],
             },
             toMixer: {
                 CHANNEL_OUT_GAIN: [
                     {
                         mixerMessage: 'f1 06 00 80 00 {channel} {level}',
-                        value: 0,
-                        type: '',
-                        min: 0,
-                        max: 1,
-                        zero: 0.75,
                     },
                 ],
-                CHANNEL_NAME: [emptyMixerMessage()],
                 PFL_ON: [
                     {
                         mixerMessage: 'f1 05 00 80 05 {channel} 01',
-                        value: 0,
-                        type: '',
-                        min: 0,
-                        max: 1,
-                        zero: 0.75,
                     },
                 ],
                 PFL_OFF: [
                     {
                         mixerMessage: 'f1 05 00 80 05 {channel} 00',
-                        value: 0,
-                        type: '',
-                        min: 0,
-                        max: 1,
-                        zero: 0.75,
                     },
                 ],
                 NEXT_SEND: [
                     {
                         mixerMessage: 'f1 06 00 80 00 {channel} {level}',
-                        value: 0,
-                        type: '',
-                        min: 0,
-                        max: 1,
-                        zero: 0.75,
                     },
                 ],
-                THRESHOLD: [emptyMixerMessage()],
-                RATIO: [emptyMixerMessage()],
-                DELAY_TIME: [emptyMixerMessage()],
-                LOW: [emptyMixerMessage()],
-                LO_MID: [emptyMixerMessage()],
-                MID: [emptyMixerMessage()],
-                HIGH: [emptyMixerMessage()],
-                AUX_LEVEL: [emptyMixerMessage()],
                 CHANNEL_MUTE_ON: [
                     {
                         mixerMessage: 'f1 05 00 80 01 {channel} 00',
-                        value: 0,
-                        type: '',
-                        min: 0,
-                        max: 1,
-                        zero: 0.75,
                     },
                 ],
                 CHANNEL_MUTE_OFF: [
                     {
                         mixerMessage: 'f1 05 00 80 01 {channel} 01',
-                        value: 0,
-                        type: '',
-                        min: 0,
-                        max: 1,
-                        zero: 0.75,
                     },
                 ],
             },
