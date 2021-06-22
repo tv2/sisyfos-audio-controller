59d6b031a5e76175d9c775ebb6a3a3a0ef409eca
olzzon <github@olzzon.dk>
2021-06-22 07:44:41 +0200

    feat: remove logging til logfile.log - as this is not used

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -1,26 +1,14 @@
 import winston from 'winston'
 const processArgs = require('minimist')(process.argv.slice(2))
 
-const loggerIp = process.env.loggerIp || processArgs.loggerIp || '0.0.0.0'
-const loggerPort = process.env.loggerPort || processArgs.loggerPort || 9200
-const loggerLevel = process.env.loggerLevel || processArgs.loggerLevel || 'info'
-const loggerFileLevel =
-    process.env.loggerFileLevel || processArgs.loggerFileLevel || 'error'
 const loggerConsoleLevel =
     process.env.loggerConsoleLevel || processArgs.loggerConsoleLevel || 'error'
-
-console.log('Elastic Ip :', loggerIp)
-console.log('Elastic Port :', loggerPort)
 
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.json(),
     transports: [
-        new winston.transports.File({
-            filename: 'logfile.log',
-            level: loggerFileLevel,
-        }), //save errors on file
-        new winston.transports.Console({ level: loggerConsoleLevel }), //save errors on file
+        new winston.transports.Console({ level: loggerConsoleLevel }), //save errors on console
     ],
 })
 
