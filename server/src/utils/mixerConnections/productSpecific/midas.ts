546367bd1d0c42a93ee4d166418a108679a3df87
olzzon <github@olzzon.dk>
2023-09-01 11:10:39 +0200

    fix: revert Midas metering back to code from last release. As metering comes in "all channels at once", so reference to Channel is way more performant.

--- server/src/utils/mixerConnections/productSpecific/midas.ts
+++ server/src/utils/mixerConnections/productSpecific/midas.ts
@@ -6,36 +6,40 @@ const calcVuLevel = (level: number) => {
     return Math.log(level) / Math.log(600) + 1
 }
 
-export const midasMeter = (mixerIndex: number, message: any, faderIndex: number) => {
+export const midasMeter = (mixerIndex: number, message: any) => {
     const DATA_OFFSET = 4
     let uint8bytes = Uint8Array.from(message[0])
     let dataview = new DataView(uint8bytes.buffer)
     let level: number
     let reductionLevel: number
+    let assignedFader: number
     let numberOfChannels =
         state.settings[0].mixers[mixerIndex].numberOfChannelsInType[0]
 
-    for (let ch = 0; ch < numberOfChannels; ch++) {
+    for (let i = 0; i < numberOfChannels; i++) {
+        assignedFader =
+            state.channels[0].chMixerConnection[mixerIndex].channel[i]
+                .assignedFader
         if (
-            faderIndex >= 0 &&
-            faderIndex < state.settings[0].numberOfFaders
+            assignedFader >= 0 &&
+            assignedFader < state.settings[0].numberOfFaders
         ) {
-            level = calcVuLevel(dataview.getFloat32(4 * ch + DATA_OFFSET, true))
+            level = calcVuLevel(dataview.getFloat32(4 * i + DATA_OFFSET, true))
             reductionLevel = dataview.getFloat32(
-                4 * (ch + 64) + DATA_OFFSET,
+                4 * (i + 64) + DATA_OFFSET,
                 true
             )
             let vuIndex: number = state.faders[0].fader[
-                faderIndex
+                assignedFader
             ].assignedChannels?.findIndex((assigned) => {
                 return (
                     assigned.mixerIndex === mixerIndex &&
-                    assigned.channelIndex === ch
+                    assigned.channelIndex === i
                 )
             })
             if (vuIndex === -1) vuIndex = 0
-            sendVuLevel(faderIndex, VuType.Channel, vuIndex, level)
-            sendVuLevel(faderIndex, VuType.Reduction, 0, 1 - reductionLevel)
+            sendVuLevel(assignedFader, VuType.Channel, vuIndex, level)
+            sendVuLevel(assignedFader, VuType.Reduction, 0, 1 - reductionLevel)
         }
     }
 }
