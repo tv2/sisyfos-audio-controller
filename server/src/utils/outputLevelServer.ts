a0b55b9ff98e65b8086bf3d48a1ff57b09c2aed0
Peter C <12292660+PeterC89@users.noreply.github.com>
2023-11-29 13:12:23 +0000

    feat: Mixer output level subscription

---
+++ server/src/utils/outputLevelServer.ts
@@ -0,0 +1,25 @@
+const sockets: Array<any> = []
+
+export function socketSubscribeOutputLevel(socket: any) {
+    const i = sockets.indexOf(socket)
+    if (i === -1) {
+        sockets.push(socket)
+    }
+}
+
+export function socketUnsubscribeOutputLevel(socket: any) {
+    const i = sockets.indexOf(socket)
+    if (i >= 0) {
+        sockets.splice(i, 1)
+    }
+}
+
+export function sendOutputLevel(
+    mixerIndex: number,
+    channelIndex: number,
+    level: number
+) {
+    sockets.forEach((socket) => {
+        socket.emit('outputLevel', mixerIndex, channelIndex, level)
+    })
+}
