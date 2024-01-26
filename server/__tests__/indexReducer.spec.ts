d79fe2ae7cd8414bceb2e7acedb53ad6a8eb074d
olzzon <github@olzzon.dk>
2024-01-26 07:37:12 +0100

    feat: Type Safety update indexReducer test

--- server/__tests__/indexReducer.spec.ts
+++ server/__tests__/indexReducer.spec.ts
@@ -1,3 +1,5 @@
+import { FaderActionTypes } from '../../shared/src/actions/faderActions'
+import { SettingsActionTypes } from '../../shared/src/actions/settingsActions'
 import indexReducer from '../../shared/src/reducers/indexReducer'
 
 import fs from 'fs'
@@ -12,9 +14,20 @@ describe('Test initialize store', () => {
         // ** Uncomment to update initial settings state:
         // let data = indexReducer(undefined, {type: ''})
         // fs.writeFileSync('__tests__/__mocks__/parsedEmptyStore-UPDATE.json', JSON.stringify(data))
+   
+        expect(
+            indexReducer(JSON.parse(parsedEmptyStoreJSON), {
+                type: SettingsActionTypes.UPDATE_SETTINGS,
+                settings: parsedInitialStore.settings,
+            })
+        ).toEqual(parsedInitialStore)
 
         expect(
-            indexReducer(JSON.parse(parsedEmptyStoreJSON), { type: '' })
+            indexReducer(undefined, {
+                type: FaderActionTypes.SET_COMPLETE_FADER_STATE,
+                allState: parsedInitialStore.faderState,
+                numberOfFaders: parsedInitialStore.settings[0].numberOfFaders,
+            })
         ).toEqual(parsedInitialStore)
     })
 })
